a9d0aec6f32ebcb629aba7c66f70014f801d009b
Saikat Talukder <saikat60mu@gmail.com>
2025-09-24 20:12:14 +0600

    Create 3. Drawing Chances.cpp

---
+++ Starters 204 (17th sept 25)/3. Drawing Chances.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for (int j = 0; j < T; j++)
+    {
+        int N;
+        int M;
+        cin >> N;
+        cin >> M;
+        string S;
+        cin >> S;
+        int AliceW = 0;
+        int BobW = 0;
+        for (int i = 0; i < M; i++)
+        {
+            if (S[i] == '1')
+            {
+                AliceW++;
+            }
+            else
+            {
+                BobW++;
+            }
+        }
+        int r = N - M;
+
+        int x = (BobW - AliceW + r) / 2;
+
+        if ((BobW - AliceW + r) % 2 == 0 && x >= 0 && x <= r)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
