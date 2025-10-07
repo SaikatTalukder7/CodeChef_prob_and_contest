4268dea846b0ee4405a0ff31f9ec80ff93e176ba
Saikat Talukder <saikat60mu@gmail.com>
2025-10-07 19:43:35 +0600

    Create P18. Cloud Watching.cpp

---
+++ Problems/P18. Cloud Watching.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int a,b;
+    cin>>a>>b;
+    if(b>= a*3)
+    {
+        cout<<"Rain"<<endl;
+    }
+    else
+    {
+        cout<<"Dry"<<endl;
+    }
+}
