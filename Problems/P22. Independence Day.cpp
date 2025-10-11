5256c0dc38784f8b089dfe4381fafa160491f696
Saikat Talukder <saikat60mu@gmail.com>
2025-10-11 22:50:40 +0600

    Create P22. Independence Day.cpp

---
+++ Problems/P22. Independence Day.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n > 15) 
+    {
+        cout << -1 << endl;
+    } 
+    else 
+    {
+        cout << 15 - n << endl;
+    }
+    return 0;
+}
