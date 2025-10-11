ad8abc1fd7696823e32012a166275c15c8e49c7a
Saikat Talukder <saikat60mu@gmail.com>
2025-10-11 22:20:40 +0600

    Create P21. Chef Bakes Cake 1.cpp

---
+++ Problems/P21. Chef Bakes Cake 1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n,m;
+    cin>>n>>m;
+    int cost = 30*n;
+    int revenue = 50*m;
+    int profit = revenue - cost;
+    cout<<profit<<endl;
+}
