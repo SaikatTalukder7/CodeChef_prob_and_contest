abd99650b56681cafbdf4413066b8d3f89a1aeb8
Saikat Talukder <saikat60mu@gmail.com>
2025-10-14 00:44:40 +0600

    Create P36. Chef and Socks.cpp

---
+++ Problems/P36. Chef and Socks.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int a,x,y;
+    cin>>a>>x>>y;
+    int sum = x+y;
+    if(sum>=a)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+}
