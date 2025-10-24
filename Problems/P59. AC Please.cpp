b293adc6108521d5d03d22e8115f57d55ad64482
Saikat Talukder <saikat60mu@gmail.com>
2025-10-24 19:46:47 +0600

    Create P59. AC Please.cpp

---
+++ Problems/P59. AC Please.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    if (n > 30) 
+    {
+        cout << "YES" << endl;
+    } 
+    else 
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
