74be53f4dba31409a8ce5c9a8068ed877686968d
Saikat Talukder <saikat60mu@gmail.com>
2025-10-25 21:32:20 +0600

    Create P63. Healthy Sleep.cpp

---
+++ Problems/P63. Healthy Sleep.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int h;
+    cin>>h;
+    if(h>8)
+    {
+        cout<<"More";
+    }
+    else if(h==8)
+    {
+        cout<<"Perfect";
+    }
+    else
+    {
+        cout<<"Less";
+    }
+}
