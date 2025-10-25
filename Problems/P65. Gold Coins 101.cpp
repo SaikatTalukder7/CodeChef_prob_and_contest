dff12819f2a64db7ecd902000522524ee1843609
Saikat Talukder <saikat60mu@gmail.com>
2025-10-25 21:36:55 +0600

    Create P65. Gold Coins 101.cpp

---
+++ Problems/P65. Gold Coins 101.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int A, B, X, Y;
+    cin >> A >> B >> X >> Y;
+    if (X > Y) 
+    {
+        cout << A << endl;
+    } 
+    else 
+    {
+        cout << B << endl;
+    }
+    return 0;
+}
